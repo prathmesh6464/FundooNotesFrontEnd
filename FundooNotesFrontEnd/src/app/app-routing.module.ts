9fc7d26e62f775c22f92707797005de3e4b1396d
prathamesh <prathmesh6464@gmail.com>
2020-05-05 21:03:16 +0530

    ADDED : fundoo notes front end project which contain login, registration, forget password, reset password files.

---
+++ FundooNotesFrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+
+const routes: Routes = [{path:'', component:LoginComponent},
+{path:'forgetpassword', component:ForgetpasswordComponent},
+{path:'registration', component:RegistrationComponent},
+{path:'resetpassword', component:ResetpasswordComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, ForgetpasswordComponent, RegistrationComponent, ResetpasswordComponent];
