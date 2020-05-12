237c023bcead2d86acbf88ef3a49ec27ad9dccaf
Prathamesh Bhiku Jadhav <37609950+prathmesh6464@users.noreply.github.com>
2020-05-12 16:31:10 +0530

    Update user-registration.service.spec.ts

--- FundooNotesFrontEnd/src/app/service/user-registration.service.spec.ts
+++ FundooNotesFrontEnd/src/app/service/user-registration.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserRegistrationService } from './user-registration.service';
+import { UserService } from './user.service';
 
 describe('UserRegistrationService', () => {
-  let service: UserRegistrationService;
+  let service: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserRegistrationService);
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
