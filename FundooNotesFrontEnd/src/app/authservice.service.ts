ebc59a9f3116b750caf1948f95036181c47dab26
prathamesh <prathmesh6464@gmail.com>
2020-05-13 08:25:35 +0530

    REFACTORED : Added automatic path of math list and mat side nav module.

--- FundooNotesFrontEnd/src/app/authservice.service.ts
+++ FundooNotesFrontEnd/src/app/authservice.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Injectable({
   providedIn: 'root'
