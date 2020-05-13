56b0c7b8c35dee7209e5640e63d86f34cbb3a3f4
prathamesh <prathmesh6464@gmail.com>
2020-05-13 08:29:05 +0530

    REFACTORED : Added path of dashboad module.

--- FundooNotesFrontEnd/src/app/app-routing.module.ts
+++ FundooNotesFrontEnd/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
 import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [{path:'', component:LoginComponent},
 {path:'user/forgetpassword', component:ForgetpasswordComponent},
 {path:'user/registration', component:RegistrationComponent},
 {path:'user/resetpassword/:token/:emailid', component:ResetpasswordComponent},
+{path:'user/dashboard', component:DashboardComponent},
 
 
 ];
