63e6dffa6609038293a2e3cca436cabbbf5772e4
prathamesh <prathmesh6464@gmail.com>
2020-05-16 03:58:32 +0530

    REFACTORED : Added link of side nav bar module.

--- FundooNotesFrontEnd/src/app/dashboard/dashboard.component.ts
+++ FundooNotesFrontEnd/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  opened = false;
 
   constructor() { }
 
