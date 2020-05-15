134009b34afd969b59680ee0aebfa008ff9bef29
prathamesh <prathmesh6464@gmail.com>
2020-05-15 20:26:42 +0530

    REFACTORED : Added router for linking dashboard page to login.

--- FundooNotesFrontEnd/src/app/login/login.component.ts
+++ FundooNotesFrontEnd/src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators, MinLengthValidator} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators, MinLengthValidator } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { UserService } from '../service/user.service';
 import { LoginDto } from '../dto/LoginDto';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -20,11 +21,11 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class LoginComponent implements OnInit {
 
-  hide=true;
+  hide = true;
   message: any;
   matcher = new MyErrorStateMatcher();
 
-  constructor(private loginApiService: UserService, private snackBar: MatSnackBar) { }
+  constructor(private loginApiService: UserService, private snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -35,22 +36,27 @@ export class LoginComponent implements OnInit {
   ]);
 
   passwordFormControl = new FormControl('',
-  Validators.required,
-  
-  );  
+    Validators.required,
+
+  );
 
   userLoginFormGroup = new FormGroup({
     userName: this.userNameFormControl,
-    password: this.passwordFormControl 
+    password: this.passwordFormControl
   });
 
 
-  
+
   loginNow() {
-    let response = this.loginApiService.userLogin(new LoginDto(this.userLoginFormGroup.get('userName').value, 
-    this.userLoginFormGroup.get('password').value)); 
-    response.subscribe((data)=>this.snackBar.open(this.message=data, this.message.action, {duration: 5000, 
-    verticalPosition: 'top', horizontalPosition: 'center', panelClass: ['red-snackbar']}));
-   
+    let response = this.loginApiService.userLogin(new LoginDto(this.userLoginFormGroup.get('userName').value,
+      this.userLoginFormGroup.get('password').value));
+    response.subscribe((data) => this.snackBar.open(this.message = data, this.message.action, {
+      duration: 5000,
+      verticalPosition: 'top', horizontalPosition: 'center', panelClass: ['red-snackbar']
+    }));
+
+    if ((this.message.toLowerCase().search("successfully")) != -1) {
+      this.router.navigate(['user/dashboard']);
     }
+  }
 }
