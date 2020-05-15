27f6ec650c9ea89a5ddff033e2d0b1cfef9d3c9f
prathamesh <prathmesh6464@gmail.com>
2020-05-15 21:11:13 +0530

    REFACTORE : Added route for dashboard.

--- FundooNotesFrontEnd/src/app/registration/registration.component.ts
+++ FundooNotesFrontEnd/src/app/registration/registration.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators, MinLengthValidator, PatternValidator, FormGroup, FormGroupName} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { FormControl, FormGroupDirective, NgForm, Validators, MinLengthValidator, PatternValidator, FormGroup, FormGroupName } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { UserDetailsDto } from '../dto/UserRegistrationDto'
 import { UserService } from '../service/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { conditionallyCreateMapObjectLiteral } from '@angular/compiler/src/render3/view/util';
+import { Router } from '@angular/router';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -26,70 +26,78 @@ export class RegistrationComponent implements OnInit {
   message: any;
   matcher = new MyErrorStateMatcher();
 
-  constructor(private service: UserService, private snackBar: MatSnackBar) { }
+  constructor(private service: UserService, private snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void { }
 
-  firstNameFormControl= new FormControl('', 
+  firstNameFormControl = new FormControl('',
     Validators.required,
     PatternValidator.apply,
   );
 
-  lastNameFormControl= new FormControl('', 
+  lastNameFormControl = new FormControl('',
     Validators.required,
     PatternValidator.apply,
   );
 
-  userNameFormControl= new FormControl('', 
+  userNameFormControl = new FormControl('',
     Validators.required,
     PatternValidator.apply,
   );
 
-  passwordFormControl= new FormControl('',
-  Validators.required,
-  MinLengthValidator.apply,
-  );
-
-  confirmPasswordFormControl= new FormControl('',
-    Validators.required,    
+  passwordFormControl = new FormControl('',
+    Validators.required,
     MinLengthValidator.apply,
-    
   );
 
-  emailFormControl= new FormControl('', 
+  confirmPasswordFormControl = new FormControl('',
+    Validators.required,
+    MinLengthValidator.apply,
+
+  );
+
+  emailFormControl = new FormControl('',
     Validators.required,
     PatternValidator.apply,
   );
 
-  mobileNumberFormControl= new FormControl('',
+  mobileNumberFormControl = new FormControl('',
     Validators.required,
     PatternValidator.apply,
   );
 
   userRegistrationForm = new FormGroup({
     firstName: this.firstNameFormControl,
-  lastName: this.lastNameFormControl, 
-  userName: this.userNameFormControl,
-  password: this.passwordFormControl,
-  confirmPassword: this.confirmPasswordFormControl,
-  mobileNumber: this.mobileNumberFormControl,
-  emailId: this.emailFormControl
+    lastName: this.lastNameFormControl,
+    userName: this.userNameFormControl,
+    password: this.passwordFormControl,
+    confirmPassword: this.confirmPasswordFormControl,
+    mobileNumber: this.mobileNumberFormControl,
+    emailId: this.emailFormControl
 
   });
-  
-  public registerNow(){
-    let response = this.service.doRegistration( new UserDetailsDto(this.userRegistrationForm.get("firstName").value,
-    this.userRegistrationForm.get("lastName").value, this.userRegistrationForm.get("userName").value,
-    this.userRegistrationForm.get("password").value, this.userRegistrationForm.get("mobileNumber").value,
-    this.userRegistrationForm.get("emailId").value));
-    
-    if ( this.userRegistrationForm.get("password").value !== this.userRegistrationForm.get("confirmPassword").value ) {
+
+  public registerNow() {
+    let response = this.service.doRegistration(new UserDetailsDto(this.userRegistrationForm.get("firstName").value,
+      this.userRegistrationForm.get("lastName").value, this.userRegistrationForm.get("userName").value,
+      this.userRegistrationForm.get("password").value, this.userRegistrationForm.get("mobileNumber").value,
+      this.userRegistrationForm.get("emailId").value));
+
+    if (this.userRegistrationForm.get("password").value !== this.userRegistrationForm.get("confirmPassword").value) {
       this.message = "Password and confirm Password not matched";
-      this.snackBar.open(this.message, this.message.action, {duration: 5000, 
-        verticalPosition: 'top', horizontalPosition: 'center', panelClass: ['red-snackbar']});
-    }else {
-      response.subscribe((data)=>this.snackBar.open(this.message=data, this.message.action, {duration: 5000, 
-        verticalPosition: 'top', horizontalPosition: 'center', panelClass: ['red-snackbar']}));
+      this.snackBar.open(this.message, this.message.action, {
+        duration: 5000,
+        verticalPosition: 'top', horizontalPosition: 'center', panelClass: ['red-snackbar']
+      });
+    } else {
+      response.subscribe((data) => this.snackBar.open(this.message = data, this.message.action, {
+        duration: 5000,
+        verticalPosition: 'top', horizontalPosition: 'center', panelClass: ['red-snackbar']
+      }));
+    }
+
+    if ((this.message.toLowerCase().search("successfully")) != -1) {
+      this.router.navigate(['user/dashboard']);
     }
   }
 }
