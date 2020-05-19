bf2ae5425b5919ebca6be8211ea09cb4762524aa
prathamesh <prathmesh6464@gmail.com>
2020-05-19 13:40:38 +0530

    REFACTORED : New folder components route imported.

--- FundooNotesFrontEnd/src/app/app.module.ts
+++ FundooNotesFrontEnd/src/app/app.module.ts
@@ -1,27 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { LoginComponent } from './components/login/login.component';
+
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from './components/service/user.service';
+import { HttpClientModule } from '@angular/common/http';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserService } from './components/service/user.service';
-import { HttpClientModule } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatExpansionModule } from '@angular/material/expansion';
+
+
+
+
+
+
 import { TextFieldModule } from '@angular/cdk/text-field';
 
 
