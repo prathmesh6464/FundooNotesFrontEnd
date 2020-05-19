d4c184e96e2d90cd2e3fc949ffb29823e9160e55
prathamesh <prathmesh6464@gmail.com>
2020-05-19 13:29:36 +0530

    DELETED : dto folder.

--- FundooNotesFrontEnd/src/app/dto/ForgetPasswordDto.ts
+++
@@ -1,6 +0,0 @@
-export class ForgetPasswordDto{
-    emailId;
-    constructor(emailId: String) {
-        this.emailId=emailId;
-    }
-}
--- FundooNotesFrontEnd/src/app/dto/LoginDto.ts
+++
@@ -1,8 +0,0 @@
-export class LoginDto {
-    userName;
-    password;
-    constructor(userName: String, password: String) {
-        this.userName=userName;
-        this.password=password;
-    }   
-}
