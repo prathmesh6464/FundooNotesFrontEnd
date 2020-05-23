df8b523aff5bdb2969e12efb9f5b5d3620eecd22
prathamesh <prathmesh6464@gmail.com>
2020-05-23 12:40:12 +0530

    REFACTORED : Changed url added notes url and user url variables.

--- FundooNotesFrontEnd/src/app/components/service/user.service.ts
+++ FundooNotesFrontEnd/src/app/components/service/user.service.ts
@@ -11,16 +11,20 @@ import { GetNotesDto } from '../dto/GetNotesDto';
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
+
+  userUrl = "http://localhost:8081/user/";
+  notesUrl = "http://localhost:8081/notes/";
 
   constructor(private http: HttpClient) { }
 
   public doRegistration(userDetailsDto: UserDetailsDto): Observable<any> {
-    return this.http.post("http://localhost:8081/user/registration", userDetailsDto);
+    return this.http.post(this.userUrl + "registration", userDetailsDto);
   }
 
   public userLogin(userLoginDto: LoginDto): Observable<any> {
-    return this.http.post("http://localhost:8081/user/login", userLoginDto);
+    return this.http.post(this.userUrl + "login", userLoginDto);
   }
 
   public logIN() {
@@ -28,18 +32,18 @@ export class UserService {
   }
 
   public userResetPassword(resetPasswordDto: ResetPasswordDto): Observable<any> {
-    return this.http.put("http://localhost:8081/user/resetPassword", resetPasswordDto);
+    return this.http.put(this.userUrl + "resetPassword", resetPasswordDto);
   }
 
   public forgetPassword(forgetPasswordDto: ForgetPasswordDto): Observable<any> {
-    return this.http.post("http://localhost:8081/user/forgetPassword", forgetPasswordDto);
+    return this.http.post(this.userUrl + "forgetPassword", forgetPasswordDto);
   }
 
   public addNote(addNoteDto: AddNoteDto): Observable<any> {
-    return this.http.post("http://localhost:8081/notes/add/" + localStorage.getItem('token'), addNoteDto);
+    return this.http.post(this.notesUrl + "add/" + localStorage.getItem('token'), addNoteDto);
   }
 
   public showNotes(): Observable<any> {
-    return this.http.get<GetNotesDto[]>("http://localhost:8081/notes/show/" + localStorage.getItem('token'));
+    return this.http.get<GetNotesDto[]>(this.notesUrl + "show/" + localStorage.getItem('token'));
   }
 }
